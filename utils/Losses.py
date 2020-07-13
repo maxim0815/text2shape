88a5d99694f61cafebc47fdcefd835e904494e05
hum <hum@tf.uni-freiburg.de>
2020-07-13 17:26:24 +0200

    triplet loss with correct inputs, added retruns for norms

--- utils/Losses.py
+++ utils/Losses.py
@@ -1,17 +1,21 @@
 import torch
 
-def TripletLoss(batch):
+def TripletLoss(a, p, n):
+
     """
     :param batch: type = list, batch of random triplets
-    :return: mean loss over batch size
+    :return: mean loss over batch size, norm values
     TODO: Parallelise using batch with type tensor.
     """
     margin = 0.7
     p_norm = 2
     loss_sum = 0
-    for i in range(len(batch)):
-        a, p, n = batch[i]
-        loss = torch.max(torch.norm(a - p, p=p_norm) - torch.norm(a - n, p=p_norm) + margin, 0)
+    norm_p = []
+    norm_n = []
+    for i in range(len(a)):
+        norm_p.append(torch.norm(a - p, p=p_norm))
+        norm_n.append(torch.norm(a - n, p=p_norm))
+        loss = torch.max(norm_p[i] - norm_n[i] + margin, 0)
         loss_sum += loss[0]
-    mean_loss = loss_sum / len(batch)
-    return mean_loss
+    mean_loss = loss_sum / len(a)
+    return mean_loss, norm_p, norm_n
