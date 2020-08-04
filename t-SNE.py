92babf613f00cf01221379ec53b6a97cfa6c9a23
markus schwegler <markus.schwegler.ms@gmail.com>
2020-08-04 20:50:45 +0200

    text tsne

--- t-SNE.py
+++ t-SNE.py
@@ -3,11 +3,12 @@ import argparse
 import yaml
 import os
 import matplotlib.pyplot as plt
-import seaborn as sns
+import torch
 
 from sklearn.manifold import TSNE
 
-from utils.ConfigParser import retrieval_config_parser
+from utils.ConfigParser import tsne_config_parser
+from models.Networks import TextEncoder, ShapeEncoder
 from dataloader.DataLoader import RetrievalLoader
 
 def parse_arguments():
@@ -16,6 +17,27 @@ def parse_arguments():
     args = parser.parse_args()
     return args
 
+def text_encoder(X, dataloader, load_directory):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    text_encoder = TextEncoder(dataloader.length_voc)
+    temp_net = torch.load(load_directory, map_location=device)
+    text_encoder = text_encoder.to(device)
+    text_encoder.load_state_dict(temp_net)
+
+    X = torch.Tensor(X).to(device=device).long()
+    X = text_encoder.forward(X)
+    return X
+
+def shape_encoder(X, dataloader, load_directory):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    shape_encoder = ShapeEncoder()
+    temp_net = torch.load(load_directory, map_location=device)
+    shape_encoder = shape_encoder.to(device)
+    shape_encoder.load_state_dict(temp_net)
+
+    X = shape_encoder.forward(X)
+    return X
+
 def plot_embedding(X, title=None):
     x_min, x_max = np.min(X, 0), np.max(X, 0)
     X = (X - x_min) / (x_max - x_min)
@@ -23,23 +45,6 @@ def plot_embedding(X, title=None):
     plt.figure()
     plt.plot(X[:,0], X[:,1], "ro")
     # plt.show()
-
-    # if hasattr(offsetbox, 'AnnotationBbox'):
-    #     # only print thumbnails with matplotlib > 1.0
-    #     shown_images = np.array([[1., 1.]])  # just something big
-    #     for i in range(X.shape[0]):
-    #         dist = np.sum((X[i] - shown_images) ** 2, 1)
-    #         if np.min(dist) < 4e-3:
-    #             # don't show points that are too close
-    #             continue
-    #         shown_images = np.r_[shown_images, [X[i]]]
-    #         imagebox = offsetbox.AnnotationBbox(
-    #             offsetbox.OffsetImage(digits.images[i], cmap=plt.cm.gray_r),
-    #             X[i])
-    #         ax.add_artist(imagebox)
-    # plt.xticks([]), plt.yticks([])
-    # if title is not None:
-    #     plt.title(title)
 
     plt.savefig('tsne.png')
 
@@ -50,14 +55,20 @@ def main(config):
 
     dataloader = RetrievalLoader(config)
 
+    n = config["hyper_parameters"]["n"]
+
     descriptions = []
-    for i in range(dataloader.get_description_length()):
-        descriptions.append(dataloader.get_description(i).tolist()[0])
+
+    rand = np.random.randint(0, dataloader.get_description_length(), n)
+    for i in range(n):
+        descriptions.append(dataloader.get_description(rand[i]).tolist()[0])
+    descriptions = text_encoder(descriptions, dataloader, load_directory[1])
+    descriptions = descriptions.cpu().detach().numpy()
     X_embedded = TSNE(n_components=2).fit_transform(descriptions)
     plot_embedding(X_embedded, "t-SNE embedding")
 
 
 if __name__ == '__main__':
     args = parse_arguments()
-    config = retrieval_config_parser(args.config)
+    config = tsne_config_parser(args.config)
     main(config)
